40f9c023375e6a3ef72b508d595f22ea4dc7294c
lykmapipo <lallyelias87@gmail.com>
2015-04-12 22:14:24 +0300

    uncomment spec bootstrap

--- test/bootstrap.spec.js
+++ test/bootstrap.spec.js
@@ -1,37 +1,37 @@
-// 'use strict';
+'use strict';
 
-// //dependencies
-// var kue = require('kue');
-// var Job = kue.Job;
-// var async = require('async');
-// var _ = require('lodash');
+//dependencies
+var kue = require('kue');
+var Job = kue.Job;
+var async = require('async');
+var _ = require('lodash');
 
-// //setup test environment
-// before(function(done) {
-//     //initializing kue default queue 
-//     //for cleaning existing jobs
-//     kue.createQueue();
+//setup test environment
+before(function(done) {
+    //initializing kue default queue 
+    //for cleaning existing jobs
+    kue.createQueue();
 
-//     //clean existing jobs
-//     async
-//         .waterfall(
-//             [
-//                 function findJobs(next) {
-//                     //TODO find a proper from..to for cleaning
-//                     //existing jobs
-//                     Job.range(0, 1000000000, 'desc', next);
-//                 },
-//                 function prepareCleaningWork(jobs, next) {
-//                     next(null, _.map(jobs, function(job) {
-//                         return job.remove.bind(job);
-//                     }));
-//                 },
+    //clean existing jobs
+    async
+        .waterfall(
+            [
+                function findJobs(next) {
+                    //TODO find a proper from..to for cleaning
+                    //existing jobs
+                    Job.range(0, 1000000000, 'desc', next);
+                },
+                function prepareCleaningWork(jobs, next) {
+                    next(null, _.map(jobs, function(job) {
+                        return job.remove.bind(job);
+                    }));
+                },
 
-//                 function cleanJobs(cleaningWork, next) {
-//                     async.parallel(cleaningWork, next);
-//                 }
-//             ],
-//             function(error, results) {
-//                 done(error, results);
-//             });
-// });
+                function cleanJobs(cleaningWork, next) {
+                    async.parallel(cleaningWork, next);
+                }
+            ],
+            function(error, results) {
+                done(error, results);
+            });
+});
