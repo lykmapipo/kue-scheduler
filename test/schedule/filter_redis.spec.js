3a25ce3d67d6453fcff1cf52dda0b1801b02b52d
andrey.goncharov <andrey.goncharov@dsr-company.com>
2015-09-02 15:12:49 +0300

    Redis pubsub message filter tests added

--- test/schedule/filter_redis.spec.js
+++ test/schedule/filter_redis.spec.js
@@ -6,24 +6,30 @@ var path = require('path');
 var kue = require(path.join(__dirname, '..', '..', 'index'));
 var faker = require('faker');
 var Queue;
+var redis = require('redis').createClient();
 
-describe('Queue#every', function() {
+describe('Queue#redisFilter', function () {
+    this.timeout(15000);
 
-    before(function(done) {
-        Queue = kue.createQueue();
-        done();
+    before(function (done) {
+        redis.flushall(function () {
+            Queue = kue.createQueue();
+            done();
+        });
     });
 
-    after(function(done) {
-        Queue.shutdown(done);
+    after(function (done) {
+        Queue.shutdown(function () {
+            redis.flushall(done);
+        });
     });
 
-    it('should be a function', function(done) {
+    it('should be a function', function (done) {
         expect(Queue.every).to.be.a('function');
         done();
     });
 
-    it('should be able to schedule a job to run every 2 seconds from now', function(done) {
+    it('should be able to filter unrelated to kue messages', function (done) {
 
         var data = {
             to: faker.internet.email()
@@ -35,7 +41,7 @@ describe('Queue#every', function() {
         };
         var runCount = 0;
 
-        Queue.process('every', function(job, finalize) {
+        Queue.process('every', function (job, finalize) {
             //increament run counts
             runCount++;
 
@@ -54,7 +60,7 @@ describe('Queue#every', function() {
         });
 
         //listen on success scheduling
-        Queue.on('schedule success', function(job) {
+        Queue.on('schedule success', function (job) {
             if (job.type === 'every') {
                 /*jshint camelcase:false */
                 expect(job.id).to.exist;
@@ -75,13 +81,64 @@ describe('Queue#every', function() {
             .backoff(backoff)
             .priority('normal');
 
-        Queue.every('2 seconds', job);
+        Queue.every('6 seconds', job);
 
-        //wait for two jobs to be runned
-        setTimeout(function() {
-            expect(runCount).to.equal(2);
+        redis.publish('__keyevent@0__:expired', 'message', function () {
+
+            //wait for two jobs to be runned
+            setTimeout(function () {
+                expect(runCount).to.equal(1);
+                done();
+            }, 9000);
+        });
+    });
+
+    it('should be able to pass related to kue messages', function (done) {
+
+        var data = {
+            to: faker.internet.email()
+        };
+
+        var backoff = {
+            delay: 60000,
+            type: 'fixed'
+        };
+        var runCount = 0;
+
+        Queue.process('every', function (job, finalize) {
+            //increament run counts
+            runCount++;
+
+            /*jshint camelcase:false */
+            expect(job.id).to.exist;
+            expect(job.type).to.equal('every');
+            expect(parseInt(job._max_attempts)).to.equal(3);
+            expect(job.data.to).to.equal(data.to);
+            expect(job.data.schedule).to.equal('RECCUR');
+
+            expect(job._backoff).to.eql(backoff);
+            expect(parseInt(job._priority)).to.equal(0);
+            /*jshint camelcase:true */
+
+            finalize();
+        });
+
+        //listen on success scheduling
+        Queue.on('schedule error', function (err) {
+            expect(err).to.match(/Invalid jobData/);
             done();
-        }, 6000);
+        });
+
+        var job = Queue
+            .createJob('every', data)
+            .attempts(3)
+            .backoff(backoff)
+            .priority('normal');
+
+        Queue.every('5 seconds', job);
+
+        redis.publish('__keyevent@0__:expired', 'q:message', function () {
+        });
     });
 
 });
