9eb52b739b24263536ff9e663abf36a97922c52e
lykmapipo <lallyelias87@gmail.com>
2015-10-27 10:56:33 +0300

    bump to v0.3.0 and review every unique jobs spec

--- test/schedule/every.spec.js
+++ test/schedule/every.spec.js
@@ -2,6 +2,7 @@
 
 //dependencies
 var expect = require('chai').expect;
+var _ = require('lodash');
 var path = require('path');
 var kue = require(path.join(__dirname, '..', '..', 'index'));
 var faker = require('faker');
@@ -9,12 +10,12 @@ var Queue;
 
 describe('Queue#every', function() {
 
-    before(function(done) {
+    beforeEach(function(done) {
         Queue = kue.createQueue();
         done();
     });
 
-    after(function(done) {
+    afterEach(function(done) {
         Queue.shutdown(done);
     });
 
@@ -23,7 +24,7 @@ describe('Queue#every', function() {
         done();
     });
 
-    it('should be able to schedule a job to run every 2 seconds from now', function(done) {
+    it('should be able to schedule a non unique job to run every 2 seconds from now', function(done) {
 
         var data = {
             to: faker.internet.email()
@@ -34,10 +35,13 @@ describe('Queue#every', function() {
             type: 'fixed'
         };
         var runCount = 0;
+        var jobs = [];
 
         Queue.process('every', function(job, finalize) {
             //increament run counts
             runCount++;
+
+            jobs.push(job);
 
             /*jshint camelcase:false */
             expect(job.id).to.exist;
@@ -80,6 +84,76 @@ describe('Queue#every', function() {
         //wait for two jobs to be runned
         setTimeout(function() {
             expect(runCount).to.equal(2);
+            var ids = _.map(jobs, 'id');
+            expect(ids[0]).to.not.equal(ids[1]);
+
+            done();
+        }, 6000);
+    });
+
+    it('should be able to schedule a unique job to run every 2 seconds from now', function(done) {
+
+        var data = {
+            to: faker.internet.email()
+        };
+
+        var backoff = {
+            delay: 60000,
+            type: 'fixed'
+        };
+        var runCount = 0;
+        var jobs = [];
+
+        Queue.process('unique_every', function(job, finalize) {
+            //increament run counts
+            runCount++;
+
+            jobs.push(job);
+
+            /*jshint camelcase:false */
+            expect(job.id).to.exist;
+            expect(job.type).to.equal('unique_every');
+            expect(parseInt(job._max_attempts)).to.equal(3);
+            expect(job.data.to).to.equal(data.to);
+            expect(job.data.schedule).to.equal('RECCUR');
+
+            expect(job._backoff).to.eql(backoff);
+            expect(parseInt(job._priority)).to.equal(0);
+            /*jshint camelcase:true */
+
+            finalize();
+        });
+
+        //listen on success scheduling
+        Queue.on('schedule success', function(job) {
+            if (job.type === 'unique_every') {
+                /*jshint camelcase:false */
+                expect(job.id).to.exist;
+                expect(job.type).to.equal('unique_every');
+                expect(parseInt(job._max_attempts)).to.equal(3);
+                expect(job.data.to).to.equal(data.to);
+                expect(job.data.schedule).to.equal('RECCUR');
+
+                expect(job._backoff).to.eql(backoff);
+                expect(parseInt(job._priority)).to.equal(0);
+                /*jshint camelcase:true */
+            }
+        });
+
+        var job = Queue
+            .createJob('unique_every', data)
+            .attempts(3)
+            .backoff(backoff)
+            .priority('normal')
+            .unique('every_mail');
+
+        Queue.every('2 seconds', job);
+
+        //wait for two jobs to be runned
+        setTimeout(function() {
+            expect(runCount).to.equal(2);
+            var ids = _.map(jobs, 'id');
+            expect(ids[0]).to.equal(ids[1]);
             done();
         }, 6000);
     });
