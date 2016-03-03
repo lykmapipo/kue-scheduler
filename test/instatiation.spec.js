887acada3d99765b6cc90087f630ff6c4e191161
lykmapipo <lallyelias87@gmail.com>
2016-03-03 09:53:54 +0300

    update instantiation specs

--- test/instatiation.spec.js
+++ test/instatiation.spec.js
@@ -9,21 +9,37 @@ var Queue;
 
 describe('Queue Job Scheduler & Listener', function() {
 
-    before(function(done) {
-        Queue = kue.createQueue();
+    afterEach(function(done) {
+        Queue.shutdown(done);
+        Queue = null;
+    });
+
+    it('should be able to instantiate kue-scheduler with custom options', function(done) {
+        var options = {
+            prefix: 'w',
+            redis: {
+                port: 6379,
+                host: '127.0.0.1',
+                db: 2
+            }
+        };
+        Queue = kue.createQueue(options);
+
+        //assert
+        expect(Queue.options).to.eql(options);
+        expect(Queue._options).to.eql(options);
+
         done();
     });
 
-    after(function(done) {
-        Queue.shutdown(done);
-    });
-
     it('should be able to instantiate scheduler redis client', function(done) {
+        Queue = kue.createQueue();
         expect(Queue._scheduler).to.exist;
         done();
     });
 
     it('should be able to instantiate expiry key listener', function(done) {
+        Queue = kue.createQueue();
         expect(Queue._listener).to.exist;
         done();
     });
